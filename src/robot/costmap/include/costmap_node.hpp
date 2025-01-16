4c83a058950fa2a1ff622ad7f5805c79289dc9ec
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2025-01-15 23:58:01 -0500

    log1

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,16 +1,29 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
-
+ 
 #include "rclcpp/rclcpp.hpp"
-
+#include "std_msgs/msg/string.hpp"
+ 
 #include "costmap_core.hpp"
-
+ 
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-
+    
+    void publishMessage();
+ 
   private:
-    robot::CostmapCore costmap_;
-};
+    // new
+    // void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
 
+    robot::CostmapCore costmap_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // new
+    // rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+    // rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
+
+};
+ 
 #endif 
